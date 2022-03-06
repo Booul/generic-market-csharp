f7f7da558cf4bceea067a44ffa05c113178b06d5
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-06 20:29:32 -0300

    feat: create output model

---
+++ Models/Output.cs
@@ -0,0 +1,10 @@
+namespace generic_market_csharp.Models
+{
+    public class Output
+    {
+        public int Id {get; set;}
+        public Product Product {get; set;}
+        public float SalePrice {get; set;}
+        public DateTime Date {get; set;}
+    }
+}
