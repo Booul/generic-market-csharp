dbe6d67befed7381ccbc6f67062370cae5848acc
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-06 20:21:44 -0300

    feat: add category model in database context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using generic_market_csharp.Models;
+
 namespace generic_market_csharp.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Category> Categories {get; set;}
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
