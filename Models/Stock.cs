b72c3c403eaf898b7179661708fddda1baa4a0e9
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-06 20:28:16 -0300

    feat: create stock model

---
+++ Models/Stock.cs
@@ -0,0 +1,9 @@
+namespace generic_market_csharp.Models
+{
+    public class Stock
+    {
+        public int Id {get; set;}
+        public Product Product {get; set;}
+        public float Amount {get; set;}
+    }
+}
