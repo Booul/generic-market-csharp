179fff31a79631a9b15d2eb400b05117acebe850
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-06 20:30:55 -0300

    feat: create sale migration

---
+++ Migrations/20220306233019_CreateSaleSchema.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace generic_market_csharp.Migrations
+{
+    public partial class CreateSaleSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Total = table.Column<float>(type: "real", nullable: false),
+                    AmountPaid = table.Column<float>(type: "real", nullable: false),
+                    Change = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sales");
+        }
+    }
+}
