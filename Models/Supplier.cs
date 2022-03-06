13e391813e27d0610a4922fdacf000ac848bae86
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-06 20:23:26 -0300

    feat: create supplier model

---
+++ Models/Supplier.cs
@@ -0,0 +1,10 @@
+namespace generic_market_csharp.Models
+{
+    public class Supplier
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Email {get; set;}
+        public string Phone {get; set;}
+    }
+}
