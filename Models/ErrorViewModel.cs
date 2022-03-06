3af4c32a4c818ac8a3dafb4b305cdd7cefdf8b35
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-06 16:50:02 -0300

    build: create project mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace generic_market_csharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
