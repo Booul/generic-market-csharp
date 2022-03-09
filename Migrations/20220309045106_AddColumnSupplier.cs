b1f3a774bac2fff9bc1e98b505a104832bf681e6
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-09 01:56:09 -0300

    feat: add column in supplier

---
+++ Migrations/20220309045106_AddColumnSupplier.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace generic_market_csharp.Migrations
+{
+    public partial class AddColumnSupplier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Suppliers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Suppliers");
+        }
+    }
+}
--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -6,5 +6,6 @@ namespace generic_market_csharp.Models
         public string Name {get; set;}
         public string Email {get; set;}
         public string Phone {get; set;}
+        public bool Status {get; set;}
     }
 }
