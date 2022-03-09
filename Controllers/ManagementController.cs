e9ab28b8980241e1b88ae5283a2c8f80eebd2bd2
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-09 03:49:48 -0300

    feat: add actions in management controller

--- Controllers/ManagementController.cs
+++ Controllers/ManagementController.cs
@@ -1,9 +1,16 @@
+using generic_market_csharp.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace generic_market_csharp.Controllers
 {
     public class ManagementController: Controller
     {
+        private readonly ApplicationDbContext database;
+
+        public ManagementController(ApplicationDbContext database) {
+            this.database = database;
+        }
+
         public IActionResult Index() {
             return View();
         }
@@ -21,6 +28,17 @@ namespace generic_market_csharp.Controllers
         }
 
         public IActionResult NewSupplier() {
+            return View();
+        }
+
+        public IActionResult Products(){
+            return View();
+        }
+
+        public IActionResult NewProduct() {
+            ViewBag.Categories = database.Categories.ToList();
+            ViewBag.Suppliers = database.Suppliers.ToList();
+
             return View();
         }
     }
