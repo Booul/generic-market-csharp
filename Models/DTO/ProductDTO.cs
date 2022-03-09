c3d83aaa3fc7205bb63db05184b55de66c017bb4
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-09 02:45:02 -0300

    feat: create product DTO

---
+++ Models/DTO/ProductDTO.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace generic_market_csharp.Models.DTO
+{
+    public class ProductDTO
+    {
+        [Required]
+        public int Id {get; set;}
+
+        [Required(ErrorMessage = "Nome do produto é obrigatório.")]
+        [StringLength(100, ErrorMessage = "Nome do produto é muito grande.")]
+        [MinLength(2, ErrorMessage = "Nome do produto é muito pequeno.")]
+        public string Name {get; set;}
+
+        [Required(ErrorMessage = "Categoria do produto é obrigatório.")]
+        public int CategoryID {get; set;}
+
+        [Required(ErrorMessage = "Fornecedor do produto é obrigatório.")]
+        public int SupplierID {get; set;}
+
+        [Required(ErrorMessage = "Preço de custo do produto é obrigatório.")]
+        public float PurchasePrice {get; set;}
+
+        [Required(ErrorMessage = "Preço de venda do produto é obrigatório.")]
+        public float SalePrice {get; set;}
+
+        [Required(ErrorMessage = "Tipo de medição do produto é obrigatório.")]
+        [Range(0, 2, ErrorMessage = "Tipo de medição inválida.")]
+        public int MeasurementType {get; set;}
+    }
+}
