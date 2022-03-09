27d6e3ac3b9b9b85e020d63b0ede14ebededa66f
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-09 02:19:28 -0300

    feat: create method save supplier

---
+++ Controllers/SupplierController.cs
@@ -0,0 +1,34 @@
+using generic_market_csharp.Models.DTO;
+using generic_market_csharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using generic_market_csharp.Data;
+
+namespace generic_market_csharp.Controllers
+{
+    public class SupplierController: Controller
+    {
+        private readonly ApplicationDbContext database;
+
+        public SupplierController(ApplicationDbContext database) {
+            this.database = database;
+        }
+
+        [HttpPost]
+        public IActionResult Save(SupplierDTO supplierDTO) {
+            if (ModelState.IsValid) {
+                Supplier supplier = new Supplier();
+                supplier.Name = supplierDTO.Name;
+                supplier.Email = supplierDTO.Email;
+                supplier.Phone = supplierDTO.Phone;
+                supplier.Status = true;
+
+                database.Suppliers.Add(supplier);
+                database.SaveChanges();
+
+                return RedirectToAction("Suppliers", "Management");
+            } else {
+                return View("../Management/NewSupplier");
+            }
+        }
+    }
+}
