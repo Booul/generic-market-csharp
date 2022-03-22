cb12f71785f2c6a5f9ccc337098a65537d3fa182
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-22 02:56:45 -0300

    fix: add columns in output model

---
+++ Migrations/20220322055500_AddColumnsOutput.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace generic_market_csharp.Migrations
+{
+    public partial class AddColumnsOutput : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Amount",
+                table: "Outputs",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SaleId",
+                table: "Outputs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Outputs_SaleId",
+                table: "Outputs",
+                column: "SaleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Outputs_Sales_SaleId",
+                table: "Outputs",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Outputs_Sales_SaleId",
+                table: "Outputs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Outputs_SaleId",
+                table: "Outputs");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Outputs");
+
+            migrationBuilder.DropColumn(
+                name: "SaleId",
+                table: "Outputs");
+        }
+    }
+}
--- Models/Output.cs
+++ Models/Output.cs
@@ -4,7 +4,9 @@ namespace generic_market_csharp.Models
     {
         public int Id {get; set;}
         public Product Product {get; set;}
+        public float Amount {get; set;}
         public float SalePrice {get; set;}
         public DateTime Date {get; set;}
+        public Sale Sale {get; set;}
     }
 }
