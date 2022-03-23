d7ec423bf1947aa7be4da7bf5d62dbda9c788cbc
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-23 03:07:15 -0300

    feat: create output controller

---
+++ Controllers/OutputController.cs
@@ -0,0 +1,38 @@
+using generic_market_csharp.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using generic_market_csharp.Data;
+using generic_market_csharp.Models;
+
+namespace generic_market_csharp.Controllers
+{
+    public class OutputController: Controller
+    {
+        private readonly ApplicationDbContext database;
+
+        public OutputController(ApplicationDbContext database) {
+            this.database = database;
+        }
+
+        [HttpPost]
+        public IActionResult SaveRange([FromBody] OutputDTO[] outputsDTO) {
+            List<Output> outputs = new List<Output>();
+
+            foreach (var outputDTO in outputsDTO)
+            {
+                Output output = new Output();
+                output.Amount = outputDTO.Amount;
+                output.SalePrice = outputDTO.SalePrice;
+                output.Sale = database.Sales.Single(sale => sale.Id == outputDTO.SaleId);
+                output.Product = database.Products.Single(product => product.Id == outputDTO.ProductId);
+                output.Date = DateTime.Now.ToUniversalTime();
+
+                outputs.Add(output);
+            }
+
+            database.Outputs.AddRange(outputs);
+            database.SaveChanges();
+
+            return Created("Output/SaveAll", outputs);
+        }
+    }
+}
