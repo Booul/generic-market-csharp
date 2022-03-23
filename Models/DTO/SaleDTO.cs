45e550b3cae8c40758bb755f23bbe136cc46d1e6
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-23 03:06:44 -0300

    feat: create sale DTO

---
+++ Models/DTO/SaleDTO.cs
@@ -0,0 +1,9 @@
+namespace generic_market_csharp.Models.DTO
+{
+    public class SaleDTO
+    {
+        public float Total {get; set;}
+        public float AmountPaid {get; set;}
+        public float Change {get; set;}
+    }
+}
