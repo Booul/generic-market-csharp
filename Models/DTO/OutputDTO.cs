823ff4e45e6056b5bc6641b5ead7ff3a7f7cb5de
Lucas Leite <lucas.leite@souunit.com.br>
2022-03-23 03:06:53 -0300

    feat: create output DTO

---
+++ Models/DTO/OutputDTO.cs
@@ -0,0 +1,10 @@
+namespace generic_market_csharp.Models.DTO
+{
+    public class OutputDTO
+    {
+        public int ProductId {get; set;}
+        public float Amount {get; set;}
+        public float SalePrice {get; set;}
+        public float SaleId {get; set;}
+    }
+}
